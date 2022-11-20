332eb402fb516f87869512e0630ba7970331cdc1
Abbe <abbe@Abbe.local>
2022-11-20 14:47:21 +0100

    schemas setup

--- backend-sanity/schemas/schema.js
+++ backend-sanity/schemas/schema.js
@@ -4,7 +4,7 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import projects from "./projects";
-import skills from "./experiences";
+import skills from "./skills";
 import workExperience from "./workExperience";
 
 // Then we give our schema to the builder and provide the result to Sanity
